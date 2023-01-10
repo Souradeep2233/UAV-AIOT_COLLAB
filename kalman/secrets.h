1f5a2d4febe180e74db850c2730f87cc0bc7738c
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2023-01-11 00:59:12 +0530

    Add files via upload

---
+++ kalman/secrets.h
@@ -0,0 +1,9 @@
+
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Airtel-MyWiFi-AMF-311WW-110F"    // replace MySSID with your WiFi network name
+#define SECRET_PASS "46c2c5a9"  // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 1873257      // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "MW6OE1EQM9OX949U"   // replace XYZ with your channel write API Key
